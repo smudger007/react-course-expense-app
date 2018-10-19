316b36265a49c1356b3cb99e789278fc184ee92b
Mark Smith <smudger007@googlemail.com>
2018-10-19 18:42:50 +0100

    setup dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
 
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -34,6 +34,7 @@ module.exports = (env) => {
                                 sourceMap: true
                             }
                         }
+                        
                     ]
                 })
             }]
@@ -44,7 +45,8 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map', 
         devServer: {
             contentBase:  path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true, 
+            publicPath: '/dist/'
         }
     };
 }
